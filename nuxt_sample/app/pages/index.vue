97c914df03286ecdf32b261ad245b3272badb923
motomichi <motomichi.motomichi@gmail.com>
2021-03-19 16:35:42 +0900

    Sassを使えるようにする

--- nuxt_sample/app/pages/index.vue
+++ nuxt_sample/app/pages/index.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>pages/index.vue</div>
+  <div class="mod-example-0001 mod-example-0001--danger">
+    <div class="mod-example-0001__first-element">
+      <ul class="mod-example-0001__list-group">
+        <li
+          class="mod-example-0001__list-item mod-example-0001__list-item--danger"
+        >
+          サンプル
+        </li>
+        <li class="mod-example-0001__list-item">サンプル</li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -8,8 +19,6 @@ import Vue from 'vue'
 export default Vue.extend({})
 </script>
 
-<style>
-.pages-index {
-  display: block;
-}
+<style lang="scss" scoped>
+@import '~/assets/scss/modules/mod-example-0001';
 </style>
