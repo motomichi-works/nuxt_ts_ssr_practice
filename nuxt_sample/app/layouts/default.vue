41768396d2904e56f265de56c8b063eea8fc62c0
motomichi <motomichi.motomichi@gmail.com>
2021-03-20 17:17:22 +0900

    headerを作成

--- nuxt_sample/app/layouts/default.vue
+++ nuxt_sample/app/layouts/default.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="l-body-inner-0001">
-    <div class="l-body-inner-0001__header-wrapper">ヘッダーが入るところ</div>
+    <div class="l-body-inner-0001__header-wrapper">
+      <header class="mod-header-0001">
+        <div class="mod-header-0001__inner">
+          <div class="mod-header-0001__logo-area">
+            <NuxtLink to="/" class="mod-header-0001__logo">ロゴ</NuxtLink>
+          </div>
+        </div>
+      </header>
+    </div>
     <div class="l-body-inner-0001__contents-wrapper">
       <Nuxt />
     </div>
@@ -11,4 +19,5 @@
 <style lang="scss" scoped>
 @import '~/assets/scss/base/global-variables/media-queries/media-queries.scss';
 @import '~/assets/scss/layouts/l-body-inner-0001.scss';
+@import '~/assets/scss/modules/mod-header-0001.scss';
 </style>
