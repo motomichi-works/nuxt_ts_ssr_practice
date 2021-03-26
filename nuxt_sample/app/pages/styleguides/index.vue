6bd783b5d19ae64444b9cddbc58045c01d6ee5e4
motomichi <motomichi.motomichi@gmail.com>
2021-03-26 20:03:03 +0900

    app/pages/styleguidesページを新規追加

---
+++ nuxt_sample/app/pages/styleguides/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="l-contents-type-0001">
+    <div class="l-contents-type-0001__heading-wrapper">
+      <h1 class="mod-heading-0001 mod-heading-0001--lv-1">
+        styleguides/index.vue
+      </h1>
+    </div>
+    <div class="l-contents-type-0001__body">
+      <section class="mod-container-0001">
+        <div class="mod-container-0001__heading-wrapper">
+          <h2 class="mod-heading-0001">見出し lv2</h2>
+        </div>
+        <div class="mod-container-0001__body">
+          ここにコンテンツのモジュールが入ります。
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style lang="scss" scoped>
+// base
+@import '~/assets/scss/base/global-variables/media-queries/media-queries.scss';
+// layouts
+@import '~/assets/scss/layouts/l-contents-type-0001';
+// modules
+@import '~/assets/scss/modules/mod-container-0001';
+@import '~/assets/scss/modules/mod-heading-0001';
+</style>
