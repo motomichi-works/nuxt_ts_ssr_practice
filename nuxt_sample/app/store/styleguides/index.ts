fccf45d25dc0fa1d86ea8c13f0304ada0818a7de
motomichi <motomichi.motomichi@gmail.com>
2021-04-28 00:54:35 +0900

    app/store/styleguides/index.tsを修正

--- nuxt_sample/app/store/styleguides/index.ts
+++ nuxt_sample/app/store/styleguides/index.ts
@@ -2,22 +2,28 @@ import { GetterTree, ActionTree, MutationTree } from 'vuex'
 
 export const state = () => ({
   fields: {
-    values: {},
+    values: {
+      'styleguides[email]': '' as string,
+      'styleguides[name_kana]': '' as string,
+    },
   },
-  realtimeErrors: {} as any,
-  exampleArray: [] as string[],
+  realtimeErrors: {
+    'styleguides[email]': [] as string[],
+    'styleguides[name_kana]': [] as string[],
+  },
+  // exampleArray: [] as string[],
   exampleString: 'exampleString',
 })
 
-export type StyleguidesState = ReturnType<typeof state>
+export type TypeOfState = ReturnType<typeof state>
 
-export const getters: GetterTree<StyleguidesState, StyleguidesState> = {
+export const getters: GetterTree<TypeOfState, TypeOfState> = {
   exampleString: (state) => state.exampleString,
   fields: (state) => state.fields,
   realtimeErrors: (state) => state.realtimeErrors,
 }
 
-export const mutations: MutationTree<StyleguidesState> = {
+export const mutations: MutationTree<TypeOfState> = {
   changeValue: (state, payload) => {
     const obj: any = {}
     obj[payload.key] = payload.value
@@ -31,4 +37,4 @@ export const mutations: MutationTree<StyleguidesState> = {
   },
 }
 
-export const actions: ActionTree<StyleguidesState, StyleguidesState> = {}
+export const actions: ActionTree<TypeOfState, TypeOfState> = {}
