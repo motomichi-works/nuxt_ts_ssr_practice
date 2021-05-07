d73de3ae963a5830d1f9adb8fa67ff8dbc603f71
motomichi <motomichi.motomichi@gmail.com>
2021-05-08 00:00:47 +0900

    Badge0001Containerコンポーネントを作成

---
+++ nuxt_sample/app/components/pages/styleguides/badge-0001-container/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>hoge</div>
+</template>
+<script lang="ts">
+import Vue, { PropType } from 'vue'
+
+export default Vue.extend({
+  name: 'Badge0001Container',
+  components: {},
+  props: {
+    moduleIds: {
+      type: Array as PropType<string[]>,
+      required: true,
+    },
+  },
+})
+</script>
+<style lang="scss" scoped>
+// base
+@import '~/assets/scss/base/global-variables/media-queries/media-queries.scss';
+// modules
+@import '~/assets/scss/modules/mod-container-0001';
+</style>
