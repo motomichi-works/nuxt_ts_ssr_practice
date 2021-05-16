0c7435847807fd10ffda6e6034201823e058336b
motomichi <motomichi.motomichi@gmail.com>
2021-05-17 01:35:21 +0900

    basic-field-unit-0001-containerからmixins/form_base.tsに処理を切り出し

--- nuxt_sample/app/components/pages/styleguides/basic-field-unit-0001-container/index.vue
+++ nuxt_sample/app/components/pages/styleguides/basic-field-unit-0001-container/index.vue
@@ -38,38 +38,16 @@
   </section>
 </template>
 <script lang="ts">
-import validate from 'validate.js'
+// import node_modules
 import Vue, { PropType } from 'vue'
+
 // mixins
 import formBase from '~/mixins/form_base'
 
-import { FieldValues, RealtimeErrors } from '~/store/styleguides/index'
-
-import constraintsBase, {
-  Constraints,
-} from '~/utils/validator/pages/styleguides/index/constraints'
-
+// components
 import BasicFieldUnit0001 from '~/components/common/basic-field-unit-0001/index.vue'
 
-const cloneDeep = require('lodash.clonedeep')
-
-type ArgsOfChangeRealtimeErrors = {
-  key: string
-  value: string[]
-}
-
-type ArgsOfChangeFieldValue = {
-  key: string
-  value: string
-}
-
-type ArgsOfValidateSingle = {
-  key: string
-  value: string
-  eventType: 'input' | 'blur'
-  validatorNames: string[]
-}
-
+// Vue.extend
 export default Vue.extend({
   name: 'BasicFieldUnit0001Container',
   components: {
@@ -85,87 +63,6 @@ export default Vue.extend({
   computed: {
     identifierStr() {
       return this.identifiers.join('_')
-    },
-    constraints(): Constraints {
-      const constraintsWithOptions = this.addOptionsToConstraints(
-        constraintsBase
-      )
-      return constraintsWithOptions
-    },
-    fieldValues() {
-      return this.$store.getters['styleguides/fieldValues'] as FieldValues
-    },
-    realtimeErrors() {
-      return this.$store.getters['styleguides/realtimeErrors'] as RealtimeErrors
-    },
-  },
-  methods: {
-    addOptionsToConstraints(args: Constraints): Constraints {
-      const constraints: Constraints = cloneDeep(args)
-      const fieldValues = this.fieldValues as FieldValues
-      const constraintsKeys = Object.keys(constraints)
-
-      constraintsKeys.forEach((constraintsKey) => {
-        const validadorNames = Object.keys(constraints[constraintsKey])
-        validadorNames.forEach((validadorName) => {
-          constraints[constraintsKey][validadorName].prevValues = fieldValues
-        })
-      })
-
-      return constraints
-    },
-    onInputField(payload: {
-      key: string
-      value: string
-      eventType: 'input'
-      validatorNames: string[]
-    }) {
-      const validationResult = this.validateSingle(payload)
-      // eslint-disable-next-line no-console
-      // console.log('validationResult: ', validationResult)
-
-      this.changeRealtimeErrors({
-        key: payload.key,
-        value: validationResult,
-      })
-      this.changeFieldValue(payload)
-    },
-    onBlurField(payload: {
-      key: string
-      value: string
-      eventType: 'blur'
-      validatorNames: string[]
-    }) {
-      const validationResult = this.validateSingle(payload)
-      // eslint-disable-next-line no-console
-      // console.log('validationResult: ', validationResult)
-
-      this.changeRealtimeErrors({
-        key: payload.key,
-        value: validationResult,
-      })
-      this.changeFieldValue(payload)
-    },
-    validateSingle(args: ArgsOfValidateSingle): string[] {
-      const constraintsKey = args.key
-      const constraint = this.constraints[constraintsKey]
-
-      args.validatorNames.forEach((validatorName) => {
-        constraint[validatorName].eventType = args.eventType
-      })
-
-      const result = validate.single(
-        args.value,
-        this.constraints[constraintsKey]
-      )
-
-      return result ?? []
-    },
-    changeRealtimeErrors(args: ArgsOfChangeRealtimeErrors): void {
-      this.$store.commit('styleguides/changeRealtimeErrors', args)
-    },
-    changeFieldValue(args: ArgsOfChangeFieldValue): void {
-      this.$store.commit('styleguides/changeFieldValue', args)
     },
   },
 })
--- nuxt_sample/app/mixins/form_base.ts
+++ nuxt_sample/app/mixins/form_base.ts
@@ -1,10 +1,119 @@
-// node_modules
+// import node_modules
+import validate from 'validate.js'
 import Vue from 'vue'
 
+// store
+import { FieldValues, RealtimeErrors } from '~/store/styleguides/index'
+
+// utils
+import constraintsBase, {
+  Constraints,
+} from '~/utils/validator/pages/styleguides/index/constraints'
+
+// require node_modules
+const cloneDeep = require('lodash.clonedeep')
+
+// define types
+type ArgsOfOnInputField = {
+  key: string
+  value: string
+  eventType: 'input'
+  validatorNames: string[]
+}
+type ArgsOfOnBlurField = {
+  key: string
+  value: string
+  eventType: 'blur'
+  validatorNames: string[]
+}
+type ArgsOfChangeRealtimeErrors = {
+  key: string
+  value: string[]
+}
+type ArgsOfChangeFieldValue = {
+  key: string
+  value: string
+}
+type ArgsOfValidateSingle = {
+  key: string
+  value: string
+  eventType: 'input' | 'blur'
+  validatorNames: string[]
+}
+
+// Vue.extend
 export default Vue.extend({
   computed: {
-    mixinComputed() {
-      return 'mixinComputed'
+    constraints(): Constraints {
+      const constraintsWithOptions = this.addOptionsToConstraints(
+        constraintsBase
+      )
+      return constraintsWithOptions
+    },
+    fieldValues() {
+      return this.$store.getters['styleguides/fieldValues'] as FieldValues
+    },
+    realtimeErrors() {
+      return this.$store.getters['styleguides/realtimeErrors'] as RealtimeErrors
+    },
+  },
+  methods: {
+    addOptionsToConstraints(args: Constraints): Constraints {
+      const constraints: Constraints = cloneDeep(args)
+      const fieldValues = this.fieldValues as FieldValues
+      const constraintsKeys = Object.keys(constraints)
+
+      constraintsKeys.forEach((constraintsKey) => {
+        const validadorNames = Object.keys(constraints[constraintsKey])
+        validadorNames.forEach((validadorName) => {
+          constraints[constraintsKey][validadorName].prevValues = fieldValues
+        })
+      })
+
+      return constraints
+    },
+    onInputField(payload: ArgsOfOnInputField) {
+      const validationResult = this.validateSingle(payload)
+      // eslint-disable-next-line no-console
+      // console.log('validationResult: ', validationResult)
+
+      this.changeRealtimeErrors({
+        key: payload.key,
+        value: validationResult,
+      })
+      this.changeFieldValue(payload)
+    },
+    onBlurField(payload: ArgsOfOnBlurField) {
+      const validationResult = this.validateSingle(payload)
+      // eslint-disable-next-line no-console
+      // console.log('validationResult: ', validationResult)
+
+      this.changeRealtimeErrors({
+        key: payload.key,
+        value: validationResult,
+      })
+      this.changeFieldValue(payload)
+    },
+    validateSingle(args: ArgsOfValidateSingle): string[] {
+      const constraintsKey = args.key
+      const constraint = this.constraints[constraintsKey]
+
+      args.validatorNames.forEach((validatorName) => {
+        constraint[validatorName].eventType = args.eventType
+      })
+
+      const result = validate.single(
+        args.value,
+        this.constraints[constraintsKey]
+      )
+
+      return result ?? []
+    },
+    changeRealtimeErrors(args: ArgsOfChangeRealtimeErrors): void {
+      this.$store.commit('styleguides/changeRealtimeErrors', args)
+    },
+    changeFieldValue(args: ArgsOfChangeFieldValue): void {
+      this.$store.commit('styleguides/changeFieldValue', args)
     },
   },
 })
