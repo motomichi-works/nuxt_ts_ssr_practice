52c835e0020584ca6ed8e77f9ec4d9e35b714d2e
motomichi <motomichi.motomichi@gmail.com>
2021-05-17 15:31:24 +0900

    app/mixins/base.tsを作成

---
+++ nuxt_sample/app/mixins/base.ts
@@ -0,0 +1,19 @@
+// Note: このmixinはapp/layoutsとapp/pages以外の全てのコンポーネントで必ず使用してください。
+
+// import node_modules
+import Vue, { PropType } from 'vue'
+
+// Vue.extend
+export default Vue.extend({
+  props: {
+    identifiers: {
+      type: Array as PropType<string[]>,
+      required: true,
+    },
+  },
+  computed: {
+    identifierStr() {
+      return this.identifiers.join('_')
+    },
+  },
+})
