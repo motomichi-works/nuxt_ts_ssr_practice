864d21df03b0e439f0e3ecd3bc075a86cfb2108b
motomichi <motomichi.motomichi@gmail.com>
2021-05-31 19:45:22 +0900

    app/utils/make_object_from_array.tsとapp/settings/pages/styleguides/index.tsを作成

---
+++ nuxt_sample/app/utils/make_object_from_array.ts
@@ -0,0 +1,14 @@
+type MyObj<K extends string, T> = { [K0 in K]: T }
+type MyKeys<T extends string> = readonly T[]
+
+export default function makeObjectFromArray<K extends string, T>(
+  myKeys: MyKeys<K>,
+  defaultValue: T
+): MyObj<K, T> {
+  const myObj = {} as MyObj<K, T>
+  for (const key of myKeys) {
+    myObj[key] = defaultValue
+  }
+
+  return myObj
+}
