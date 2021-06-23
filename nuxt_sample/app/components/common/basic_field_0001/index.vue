5dae4d654e444d8ebc8365515a9b436f0abe7cee
motomichi <motomichi.motomichi@gmail.com>
2021-06-23 16:08:36 +0900

    mixin/free_input_field.tsを作成して適用

--- nuxt_sample/app/components/common/basic_field_0001/index.vue
+++ nuxt_sample/app/components/common/basic_field_0001/index.vue
@@ -22,87 +22,16 @@
 
 <script lang="ts">
 // import node_modules
-import Vue, { PropType } from 'vue'
-import { IconDefinition } from '@fortawesome/fontawesome-common-types/index'
+import Vue from 'vue'
 
 // mixins
 import every from '~/mixins/common/every'
-import fieldBase from '~/mixins/common/field_base'
+import freeInputField from '~/mixins/common/free_input_field'
 
 // Vue.extend
 export default Vue.extend({
   name: 'BasicField0001',
-  mixins: [every, fieldBase],
-  props: {
-    modifiers: {
-      type: Array as PropType<string[]>,
-      required: false,
-      default: () => ['basic-field-0001--size-md'],
-    },
-    placeholder: {
-      type: String as PropType<string>,
-      required: false,
-      default: '',
-    },
-    maxlength: {
-      type: String as PropType<string>,
-      required: false,
-      default: '',
-    },
-    leftIcon: {
-      type: Object as PropType<IconDefinition>,
-      required: false,
-      default: () => null,
-    },
-    rightIcon: {
-      type: Object as PropType<IconDefinition>,
-      required: false,
-      default: () => null,
-    },
-  },
-  computed: {
-    classes() {
-      const classes = ['basic-field-0001', ...this.modifiers]
-
-      if (this.leftIcon !== null) {
-        classes.push('basic-field-0001--icon-absolute-left')
-      }
-      if (this.rightIcon !== null) {
-        classes.push('basic-field-0001--icon-absolute-right')
-      }
-      if ((this as any).hasRealtimeErrors) {
-        classes.push('basic-field-0001--invalid')
-      }
-
-      return classes
-    },
-    isVisibleLeftIcon() {
-      return this.leftIcon !== null
-    },
-    isVisibleRightIcon() {
-      return this.rightIcon !== null
-    },
-    bindValue: {
-      get(): string {
-        return (this as any).value
-      },
-      set(value: string) {
-        this.onInput(value)
-      },
-    },
-  },
-  methods: {
-    onInput(value: string) {
-      const payload = {
-        key: (this as any).nameProperty,
-        value,
-        eventType: 'input',
-        validatorNames: (this as any).validatorNames,
-      } as const
-
-      this.$emit('on-input-field', payload)
-    },
-  },
+  mixins: [every, freeInputField],
 })
 </script>
 
---
+++ nuxt_sample/app/mixins/common/free_input_field.ts
@@ -0,0 +1,81 @@
+// import node_modules
+import Vue, { PropType } from 'vue'
+import { IconDefinition } from '@fortawesome/fontawesome-common-types/index'
+
+// mixins
+import fieldBase from '~/mixins/common/field_base'
+
+// Vue.extend
+export default Vue.extend({
+  mixins: [fieldBase],
+  props: {
+    modifiers: {
+      type: Array as PropType<string[]>,
+      required: false,
+      default: () => ['basic-field-0001--size-md'],
+    },
+    placeholder: {
+      type: String as PropType<string>,
+      required: false,
+      default: '',
+    },
+    maxlength: {
+      type: String as PropType<string>,
+      required: false,
+      default: '',
+    },
+    leftIcon: {
+      type: Object as PropType<IconDefinition>,
+      required: false,
+      default: () => null,
+    },
+    rightIcon: {
+      type: Object as PropType<IconDefinition>,
+      required: false,
+      default: () => null,
+    },
+  },
+  computed: {
+    classes() {
+      const classes = ['basic-field-0001', ...this.modifiers]
+
+      if (this.leftIcon !== null) {
+        classes.push('basic-field-0001--icon-absolute-left')
+      }
+      if (this.rightIcon !== null) {
+        classes.push('basic-field-0001--icon-absolute-right')
+      }
+      if ((this as any).hasRealtimeErrors) {
+        classes.push('basic-field-0001--invalid')
+      }
+
+      return classes
+    },
+    isVisibleLeftIcon() {
+      return this.leftIcon !== null
+    },
+    isVisibleRightIcon() {
+      return this.rightIcon !== null
+    },
+    bindValue: {
+      get(): string {
+        return (this as any).value
+      },
+      set(value: string) {
+        this.onInput(value)
+      },
+    },
+  },
+  methods: {
+    onInput(value: string) {
+      const payload = {
+        key: (this as any).nameProperty,
+        value,
+        eventType: 'input',
+        validatorNames: (this as any).validatorNames,
+      } as const
+
+      this.$emit('on-input-field', payload)
+    },
+  },
+})
