dafeba07dc67393340664682a98a53e006cdb271
motomichi <motomichi.motomichi@gmail.com>
2021-06-23 17:22:51 +0900

    constraints_base_of_all.tsを作成

---
+++ nuxt_sample/app/utils/validator/constraints_base_of_all.ts
@@ -0,0 +1,7 @@
+// utils
+import { constraintsBase as styleguides } from '~/utils/validator/pages/styleguides/index/constraints'
+
+// NOTE: namespaceをconstraintsBaseOfAllのキーとして使用します。
+export const constraintsBaseOfAll = {
+  styleguides,
+}
