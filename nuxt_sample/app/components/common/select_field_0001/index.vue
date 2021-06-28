9426e8874522f4efad07d4d557242b76c379325f
motomichi <motomichi.motomichi@gmail.com>
2021-06-28 16:48:05 +0900

    app/mixins/common/select_field_radio_field.tsをSelectField0001コンポーネントから切り出し

--- nuxt_sample/app/components/common/select_field_0001/index.vue
+++ nuxt_sample/app/components/common/select_field_0001/index.vue
@@ -31,78 +31,16 @@
 
 <script lang="ts">
 // import node_modules
-import Vue, { PropType } from 'vue'
+import Vue from 'vue'
 
 // mixins
 import everyChild from '~/mixins/common/every_child'
-import fieldBase from '~/mixins/common/field_base'
-
-// types
-import { PayloadForOnChangeField } from '~/types/payload_for_on_change_field'
-export type Option = {
-  label: string
-  value: string
-}
+import selectFieldRadioField from '~/mixins/common/select_field_radio_field'
 
 // Vue.extend
 export default Vue.extend({
   name: 'SelectField0001',
-  mixins: [everyChild, fieldBase],
-  props: {
-    modifiers: {
-      type: Array as PropType<string[]>,
-      required: false,
-      default: () => ['select-field-0001--size-md'],
-    },
-    options: {
-      type: Array as PropType<Option[]>,
-      required: false,
-      default: () => [{ label: '選択してください', value: '' }],
-    },
-  },
-  data() {
-    return {
-      refreshKey: 0,
-    }
-  },
-  computed: {
-    classes() {
-      const classes = ['select-field-0001', ...this.modifiers]
-
-      if ((this as any).hasRealtimeErrors) {
-        classes.push('select-field-0001--invalid')
-      }
-
-      return classes
-    },
-    bindValue: {
-      get(): string {
-        return (this as any).value
-      },
-      set(value: string) {
-        const payload: PayloadForOnChangeField = {
-          key: (this as any).nameProperty,
-          value,
-          eventType: 'change',
-          validatorNames: (this as any).validatorNames,
-        }
-
-        ;(this as any).emitOnChangeField(payload)
-      },
-    },
-    selectedLabel() {
-      const selectedOption = this.options.find((option) => {
-        return option.value === (this as any).value
-      })
-
-      return selectedOption ? selectedOption.label : ''
-    },
-  },
-  watch: {
-    options() {
-      ;(this.refreshKey as any)++
-    },
-  },
+  mixins: [everyChild, selectFieldRadioField],
 })
 </script>
 
---
+++ nuxt_sample/app/mixins/common/select_field_radio_field.ts
@@ -0,0 +1,72 @@
+// import node_modules
+import Vue, { PropType } from 'vue'
+
+// mixins
+import fieldBase from '~/mixins/common/field_base'
+
+// types
+import { PayloadForOnChangeField } from '~/types/payload_for_on_change_field'
+export type Option = {
+  label: string
+  value: string
+}
+
+// Vue.extend
+export default Vue.extend({
+  mixins: [fieldBase],
+  props: {
+    modifiers: {
+      type: Array as PropType<string[]>,
+      required: false,
+      default: () => ['select-field-0001--size-md'],
+    },
+    options: {
+      type: Array as PropType<Option[]>,
+      required: false,
+      default: () => [{ label: '選択してください', value: '' }],
+    },
+  },
+  data() {
+    return {
+      refreshKey: 0,
+    }
+  },
+  computed: {
+    classes() {
+      const classes = ['select-field-0001', ...this.modifiers]
+
+      if ((this as any).hasRealtimeErrors) {
+        classes.push('select-field-0001--invalid')
+      }
+
+      return classes
+    },
+    bindValue: {
+      get(): string {
+        return (this as any).value
+      },
+      set(value: string) {
+        const payload: PayloadForOnChangeField = {
+          key: (this as any).nameProperty,
+          value,
+          eventType: 'change',
+          validatorNames: (this as any).validatorNames,
+        }
+
+        ;(this as any).emitOnChangeField(payload)
+      },
+    },
+    selectedLabel() {
+      const selectedOption = this.options.find((option) => {
+        return option.value === (this as any).value
+      })
+
+      return selectedOption ? selectedOption.label : ''
+    },
+  },
+  watch: {
+    options() {
+      ;(this.refreshKey as any)++
+    },
+  },
+})
