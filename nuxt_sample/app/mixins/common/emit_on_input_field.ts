47f51aac1bde5aeb00977d6bd6b31146dbd94f8a
motomichi <motomichi.motomichi@gmail.com>
2021-06-28 11:38:56 +0900

    app/mixins/common/emit_on_input_field.tsを作成

---
+++ nuxt_sample/app/mixins/common/emit_on_input_field.ts
@@ -0,0 +1,14 @@
+// import node_modules
+import Vue from 'vue'
+
+// types
+import { PayloadForOnInputField } from "~/types/payload_for_on_input_field"
+
+// Vue.extend
+export default Vue.extend({
+  methods: {
+    emitOnInputField(payload: PayloadForOnInputField) {
+      this.$emit('on-input-field', payload)
+    },
+  },
+})
