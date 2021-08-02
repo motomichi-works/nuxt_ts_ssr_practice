b646ec49a335f54338e4a01ffe819963e949d7fc
motomichi <motomichi.motomichi@gmail.com>
2021-08-02 18:52:23 +0900

    CombinationField型を修正

--- nuxt_sample/app/types/combination_field.d.ts
+++ nuxt_sample/app/types/combination_field.d.ts
@@ -2,6 +2,7 @@ export type CombinationField = {
   namespace: string
   sharedKey: string
   realtimeErrors: string[]
+  value: string
   combinationFieldValueObj: { [key: string]: string }
   isTainted: boolean
   eventType: 'input' | 'change' | 'blur'
