3ae09805ff1f488b5a6d994738937e7d958f60bb
motomichi <motomichi.motomichi@gmail.com>
2021-08-02 18:24:44 +0900

    変数名変更「constraintsKey」->「sharedKey」

--- nuxt_sample/app/utils/validate_single.ts
+++ nuxt_sample/app/utils/validate_single.ts
@@ -14,8 +14,8 @@ export default function validateSingle<C extends { [key: string]: any }>(
   payload: PayloadForValidateSingle,
   constraints: C
 ): string[] {
-  const constraintsKey = payload.sharedKey
-  const constraint = constraints[payload.namespace][constraintsKey]
+  const sharedKey = payload.sharedKey
+  const constraint = constraints[payload.namespace][sharedKey]
 
   // 動的なオプションに依存しているvalidatorにはオプションを追加する
   payload.validatorNamesThatDependsOnDynamicOptions.forEach((validatorName) => {
