1216d3f2a13746b2cf33bd358768bdf0db40a2b0
motomichi <motomichi.motomichi@gmail.com>
2021-08-09 21:15:04 +0900

    specファイルにコメントを追加

--- nuxt_sample/test/app/pages/styleguides/index.spec.ts
+++ nuxt_sample/test/app/pages/styleguides/index.spec.ts
@@ -1,13 +1,19 @@
+// node_modulesをimport
 import { createLocalVue, mount } from '@vue/test-utils'
 import Vuex from 'vuex'
+
+// Componentをimport
 import Component from '@/app/pages/styleguides/index.vue'
 
+// 自分で定義したtest_utilsをimport
 // @ts-ignore
 import { createOptionsForStoreConstructor } from '@/test/test_utils/create_options_for_store_constructor.ts'
 
+// localVueを初期化
 const localVue = createLocalVue()
 localVue.use(Vuex)
 
+// テストする
 describe('StyleguidesIndex', () => {
   test('正常にmount完了する', () => {
     const activationKeys = ['styleguides'] as const
