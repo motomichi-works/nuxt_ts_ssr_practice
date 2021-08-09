b825bccc207d6b56d0b391ae7be4df7996358bd0
motomichi <motomichi.motomichi@gmail.com>
2021-08-09 21:20:38 +0900

    createStore()とcreateWrapper()を定義して、コメントを整理

--- nuxt_sample/test/app/pages/styleguides/index.spec.ts
+++ nuxt_sample/test/app/pages/styleguides/index.spec.ts
@@ -13,18 +13,28 @@ import { createOptionsForStoreConstructor } from '@/test/test_utils/create_optio
 const localVue = createLocalVue()
 localVue.use(Vuex)
 
-// テストする
+// createStore関数を定義（createStore関数は無理に外部に切り出さずspecファイルごとに定義する）
+function createStore() {
+  const activationKeys = ['styleguides'] as const
+  const optionsForStoreConstructor = createOptionsForStoreConstructor(
+    activationKeys
+  )
+  return new Vuex.Store(optionsForStoreConstructor)
+}
+
+// createWrapper関数を定義（createWrapper関数は無理に外部に切り出さずspecファイルごとに定義する）
+function createWrapper(store: any) {
+  return mount(Component, {
+    store,
+    localVue,
+  })
+}
+
+// テストケース
 describe('StyleguidesIndex', () => {
   test('正常にmount完了する', () => {
-    const activationKeys = ['styleguides'] as const
-    const optionsForStoreConstructor = createOptionsForStoreConstructor(
-      activationKeys
-    )
-    const store = new Vuex.Store(optionsForStoreConstructor)
-    const wrapper = mount(Component, {
-      store,
-      localVue,
-    })
+    const store = createStore()
+    const wrapper = createWrapper(store)
 
     expect(wrapper.vm).toBeTruthy()
   })
