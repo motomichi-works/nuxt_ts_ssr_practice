589d2a6246aaf4a9d67ef2c1c938c8a1dc765ea7
motomichi <motomichi.motomichi@gmail.com>
2021-08-09 18:24:09 +0900

    rootStoreもimportするようにしてtest/test_utils/create_store.tsを少し整理

--- nuxt_sample/test/test_utils/create_store.ts
+++ nuxt_sample/test/test_utils/create_store.ts
@@ -1,6 +1,14 @@
 import Vuex from 'vuex'
 
 import {
+  state as rootState,
+  getters as rootGetters,
+  mutations as rootMutations,
+  actions as rootActions,
+  // @ts-ignore
+} from '@/app/store/index.ts'
+
+import {
   state as styleguidesIndexState,
   getters as styleguidesIndexGetters,
   mutations as styleguidesIndexMutations,
@@ -9,17 +17,35 @@ import {
 } from '@/app/store/styleguides/index.ts'
 
 export const createStore = () => {
-  const store = new Vuex.Store({
-    modules: {
-      styleguides: {
-        namespaced: true,
-        state: styleguidesIndexState,
-        getters: styleguidesIndexGetters,
-        mutations: styleguidesIndexMutations,
-        actions: styleguidesIndexActions,
-      },
+  const keysToActivate = [
+    'styleguides',
+  ] as const
+
+  const allModules = {
+    styleguides: {
+      namespaced: true,
+      state: styleguidesIndexState,
+      getters: styleguidesIndexGetters,
+      mutations: styleguidesIndexMutations,
+      actions: styleguidesIndexActions,
     },
-  })
+  }
+
+  const pickedModules: any = {}
+
+  if (keysToActivate.includes('styleguides')) {
+    pickedModules.styleguides = allModules.styleguides
+  }
+
+  const optionsForStoreConstructor = {
+    state: rootState,
+    getters: rootGetters,
+    mutations: rootMutations,
+    actions: rootActions,
+    modules: pickedModules,
+  }
+
+  const store = new Vuex.Store(optionsForStoreConstructor)
 
   return store
 }
