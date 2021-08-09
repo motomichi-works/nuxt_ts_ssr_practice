b7f525d090c11d8a3723b428c66d7b729b42e9ce
motomichi <motomichi.motomichi@gmail.com>
2021-08-09 17:28:06 +0900

    createWrapper()を外部ファイルに切り出し

--- nuxt_sample/test/app/pages/styleguides/index.spec.ts
+++ nuxt_sample/test/app/pages/styleguides/index.spec.ts
@@ -1,20 +1,11 @@
-import { createLocalVue, mount } from '@vue/test-utils'
+import { createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
 import Component from '@/app/pages/styleguides/index.vue'
 
 // @ts-ignore
 import { createStore } from '@/test/test_utils/create_store.ts'
-
-type OptionsForMount = {
-  localVue: any
-  store?: any
-}
-
-function createWrapper<T> (Component: T, optionsForMount: OptionsForMount) {
-  const wrapper = mount(Component, optionsForMount)
-
-  return wrapper
-}
+// @ts-ignore
+import { createWrapper } from '@/test/test_utils/create_wrapper.ts'
 
 const localVue = createLocalVue()
 localVue.use(Vuex)
---
+++ nuxt_sample/test/test_utils/create_wrapper.ts
@@ -0,0 +1,12 @@
+import { mount } from '@vue/test-utils'
+
+type OptionsForMount = {
+  localVue: any
+  store?: any
+}
+
+export function createWrapper<T> (Component: T, optionsForMount: OptionsForMount) {
+  const wrapper = mount(Component, optionsForMount)
+
+  return wrapper
+}
