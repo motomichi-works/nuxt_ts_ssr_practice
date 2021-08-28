d988729d9fb400655b91e596cd7eb8d60e2ccc4a
motomichi <motomichi.motomichi@gmail.com>
2021-08-28 19:54:05 +0900

    コンポーネントのnameが間違っていて重複していた箇所を修正

--- nuxt_sample/app/components/pages/index/qiita_items_container/index.vue
+++ nuxt_sample/app/components/pages/index/qiita_items_container/index.vue
@@ -32,7 +32,7 @@ import everyChild from '~/mixins/common/every_child'
 
 // Vue.extend
 export default Vue.extend({
-  name: 'Badge0001Container',
+  name: 'QiitaItemsContainer',
   components: {
     Heading0001,
   },
