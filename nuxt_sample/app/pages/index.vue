71919f8930f2c07b55d50b660c08fbf012fa41de
motomichi <motomichi.motomichi@gmail.com>
2021-08-28 20:27:15 +0900

    これまでトップページにあったコンテンツをqiita_items/indexに移動して、トップページにはナビを作成

--- nuxt_sample/app/pages/index.vue
+++ nuxt_sample/app/pages/index.vue
@@ -1,39 +1,25 @@
 <template>
-  <PagePresentational0001 page-heading-text="index.vue">
-    <QiitaItemsContainer
-      :identifiers="['QiitaItemsContainer']"
-      :qiita-items="qiitaItems"
-    />
+  <PagePresentational0001 page-heading-text="トップページ">
+    <div>
+      <ul>
+        <li><NuxtLink to="/qiita_items/">qiita_items/index</NuxtLink></li>
+        <li><NuxtLink to="/styleguides/">styleguides/index</NuxtLink></li>
+      </ul>
+    </div>
   </PagePresentational0001>
 </template>
 
 <script lang="ts">
 // import node_modules
-import { Context } from '@nuxt/types'
 import Vue from 'vue'
 
-// import components/common
-import PagePresentational0001 from '~/components/common/page_presentational_0001/index.vue'
-
 // import components/pages
-import QiitaItemsContainer from '~/components/pages/index/qiita_items_container/index.vue'
+import PagePresentational0001 from '~/components/common/page_presentational_0001/index.vue'
 
 // Vue.extend
 export default Vue.extend({
   components: {
     PagePresentational0001,
-    QiitaItemsContainer,
-  },
-  async asyncData(context: Context): Promise<any> {
-    const { app } = context
-    const qiitaItems = await app.$axios.$get(
-      'https://qiita.com/api/v2/items?query=tag:nuxt.js'
-    )
-    return {
-      qiitaItems,
-    }
   },
 })
 </script>
-
-<style lang="scss" scoped></style>
---
+++ nuxt_sample/app/pages/qiita_items/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <PagePresentational0001 page-heading-text="index.vue">
+    <QiitaItemsContainer
+      :identifiers="['QiitaItemsContainer']"
+      :qiita-items="qiitaItems"
+    />
+  </PagePresentational0001>
+</template>
+
+<script lang="ts">
+// import node_modules
+import { Context } from '@nuxt/types'
+import Vue from 'vue'
+
+// import components/common
+import PagePresentational0001 from '~/components/common/page_presentational_0001/index.vue'
+
+// import components/pages
+import QiitaItemsContainer from '~/components/pages/qiita_items/qiita_items_container/index.vue'
+
+// Vue.extend
+export default Vue.extend({
+  components: {
+    PagePresentational0001,
+    QiitaItemsContainer,
+  },
+  async asyncData(context: Context): Promise<any> {
+    const { app } = context
+    const qiitaItems = await app.$axios.$get(
+      'https://qiita.com/api/v2/items?query=tag:nuxt.js'
+    )
+    return {
+      qiitaItems,
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped></style>
