0df5cd5ca66f5ea91a7982e0260ca4f23a5b4f81
motomichi <motomichi.motomichi@gmail.com>
2021-09-04 16:15:31 +0900

    List0001コンポーネントを作成してOrgQiitaItemsに適用

---
+++ nuxt_sample/app/components/common/molecules_and_atoms/list_0001/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <ul class="list0001">
+    <li
+      v-for="(item, index) in items"
+      :key="`${item}${index}`"
+      class="list0001__item"
+    >
+      {{ item }}
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+// import node_modules
+import Vue, { PropType } from 'vue'
+
+// mixins
+import everyChild from '~/mixins/common/unclassifiable/every_child'
+
+// Vue.extend
+export default Vue.extend({
+  name: 'List0001',
+  mixins: [everyChild],
+  props: {
+    items: {
+      type: Array as PropType<string[]>,
+      required: false,
+      default: () => [],
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.list0001 {
+  font-size: 16px;
+  .list0001__item {
+    padding-left: 1em;
+    &::before {
+      content: '・';
+      margin-left: -1em;
+    }
+  }
+  .list0001__item + .list0001__item {
+    margin-top: 0.25em;
+  }
+}
+</style>
--- nuxt_sample/app/components/qiita_items/index/org_containers/org_qiita_items/index.vue
+++ nuxt_sample/app/components/qiita_items/index/org_containers/org_qiita_items/index.vue
@@ -3,16 +3,11 @@
     :identifiers="[...identifiers, 'OrgPresentational']"
     org-presentational-heading-text="qiitaのAPIから取得したitem情報のサンプル"
   >
-    <ul v-if="qiitaItems.length > 0">
-      <li
-        v-for="(item, index) in qiitaItems"
-        :key="`item.created_at__${index}`"
-      >
-        <p v-if="item.created_at">
-          {{ item.created_at }}
-        </p>
-      </li>
-    </ul>
+    <List0001
+      v-if="createdAtItems.length > 0"
+      :identifiers="[...identifiers, 'CreatedAtList']"
+      :items="createdAtItems"
+    />
   </OrgPresentational0001>
 </template>
 <script lang="ts">
@@ -21,6 +16,7 @@ import Vue, { PropType } from 'vue'
 
 // import components/org_presentationals
 import OrgPresentational0001 from '~/components/common/org_presentationals/org_presentational_0001/index.vue'
+import List0001 from '~/components/common/molecules_and_atoms/list_0001/index.vue'
 
 // mixins
 import everyChild from '~/mixins/common/unclassifiable/every_child'
@@ -30,6 +26,7 @@ export default Vue.extend({
   name: 'OrgQiitaItems',
   components: {
     OrgPresentational0001,
+    List0001,
   },
   mixins: [everyChild],
   props: {
@@ -37,6 +34,13 @@ export default Vue.extend({
       type: Array as PropType<any[]>,
       required: false,
       default: () => [],
+    },
+  },
+  computed: {
+    createdAtItems() {
+      return this.qiitaItems.map((item) => {
+        return item.created_at
+      })
     },
   },
 })
