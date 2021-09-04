cd522740a8ac5c47e98f01b3ffebe109f064c1f7
motomichi <motomichi.motomichi@gmail.com>
2021-09-04 17:20:23 +0900

    List0002コンポーネントを作成してindexページに適用

---
+++ nuxt_sample/app/components/common/molecules_and_atoms/list_0002/index.vue
@@ -0,0 +1,78 @@
+<template>
+  <ul class="list0002">
+    <li
+      v-for="(innerItems, lv1Index) in items"
+      :key="`${JSON.stringify(innerItems)}${lv1Index}`"
+      class="list0002__item"
+    >
+      <template v-for="(element, lv2Index) in innerItems">
+        <NuxtLink
+          v-if="element.tagName === 'NuxtLink'"
+          :key="`${JSON.stringify(innerItems)}${lv2Index}`"
+          :class="element.className"
+          :to="element.to"
+          v-text="element.text"
+        />
+        <component
+          :is="element.tagName"
+          v-else
+          :key="`${JSON.stringify(innerItems)}${lv2Index}`"
+          :class="element.className"
+          v-text="element.text"
+        />
+      </template>
+    </li>
+  </ul>
+</template>
+
+<script lang="ts">
+// import node_modules
+import Vue, { PropType } from 'vue'
+
+// mixins
+import everyChild from '~/mixins/common/unclassifiable/every_child'
+
+type InnerItem =
+  | {
+      tagName: 'NuxtLink'
+      text: string
+      className: string
+      to: string
+    }
+  | {
+      tagName: 'div' | 'span'
+      text: string
+      className: string
+    }
+
+type InnerItems = InnerItem[]
+
+// Vue.extend
+export default Vue.extend({
+  name: 'List0002',
+  mixins: [everyChild],
+  props: {
+    items: {
+      type: Array as PropType<InnerItems[]>,
+      required: false,
+      default: () => [],
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.list0002 {
+  font-size: 16px;
+  .list0002__item {
+    padding-left: 1em;
+    &::before {
+      content: '・';
+      margin-left: -1em;
+    }
+  }
+  .list0002__item + .list0002__item {
+    margin-top: 0.25em;
+  }
+}
+</style>
--- nuxt_sample/app/pages/index.vue
+++ nuxt_sample/app/pages/index.vue
@@ -1,11 +1,26 @@
 <template>
-  <PagePresentational0001 page-heading-text="トップページ">
-    <div>
-      <ul>
-        <li><NuxtLink to="/qiita_items/">qiita_items/index</NuxtLink></li>
-        <li><NuxtLink to="/styleguides/">styleguides/index</NuxtLink></li>
-      </ul>
-    </div>
+  <PagePresentational0001 page-heading-text="index">
+    <List0002
+      :identifiers="['Nav']"
+      :items="[
+        [
+          {
+            tagName: 'NuxtLink',
+            text: 'styleguides/index',
+            to: '/styleguides/',
+            className: '',
+          },
+        ],
+        [
+          {
+            tagName: 'NuxtLink',
+            text: 'qiita_items/index',
+            to: '/qiita_items/',
+            className: '',
+          },
+        ],
+      ]"
+    />
   </PagePresentational0001>
 </template>
 
@@ -15,11 +30,13 @@ import Vue from 'vue'
 
 // import components/orgs
 import PagePresentational0001 from '~/components/common/page_presentationals/page_presentational_0001/index.vue'
+import List0002 from '~/components/common/molecules_and_atoms/list_0002/index.vue'
 
 // Vue.extend
 export default Vue.extend({
   components: {
     PagePresentational0001,
+    List0002,
   },
 })
 </script>
