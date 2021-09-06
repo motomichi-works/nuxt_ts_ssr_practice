3026f82c7b09f3fdd3e5d935e8a86ba0d9c6ded3
motomichi <motomichi.motomichi@gmail.com>
2021-09-06 21:21:33 +0900

    org_footer_0001.vueをorg_footer_0001_container.vueとorg_footer_0001_presentational.vueに分割

---
+++ nuxt_sample/app/components/common/org_containers/org_footer_0001_container.vue
@@ -0,0 +1,23 @@
+<template>
+  <OrgFooter0001Presentational :identifiers="identifiers" />
+</template>
+
+<script lang="ts">
+// import node_modules
+import Vue from 'vue'
+
+// import components
+import OrgFooter0001Presentational from '~/components/common/org_presentationals/org_footer_0001_presentational.vue'
+
+// mixins
+import { everyChild } from '~/mixins/common/unclassifiable/every_child'
+
+// Vue.extend
+export default Vue.extend({
+  name: 'OrgFooter0001Container',
+  components: {
+    OrgFooter0001Presentational,
+  },
+  mixins: [everyChild],
+})
+</script>
